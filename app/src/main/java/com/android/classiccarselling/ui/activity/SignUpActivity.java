446563ea0229cda385cbc9c0911af840b8d6f906
Jalees Mukarram <jaleesmukarram@gmail.com>
2021-11-16 22:04:33 +0500

    Sign Up Code Added

--- app/src/main/java/com/android/classiccarselling/ui/activity/SignUpActivity.java
+++ app/src/main/java/com/android/classiccarselling/ui/activity/SignUpActivity.java
@@ -1,16 +1,48 @@
 package com.android.classiccarselling.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.android.classiccarselling.R;
+import com.android.classiccarselling.databinding.ActivitySignUpBinding;
+import com.android.classiccarselling.interfaces.CustomHooks;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements CustomHooks {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void callHooks() {
+
+        initViews();
+        initListeners();
+    }
+
+    @Override
+    public void handleIntent() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+
+    @Override
+    public void observe() {
+
     }
 }
