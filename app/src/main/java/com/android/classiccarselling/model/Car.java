77e52a4acf8d93fb637e570a575f6a6e286acd07
Jalees Mukarram <jaleesmukarram@gmail.com>
2021-11-18 01:24:07 +0500

    Car Model adjusted

--- app/src/main/java/com/android/classiccarselling/model/Car.java
+++ app/src/main/java/com/android/classiccarselling/model/Car.java
@@ -1,17 +1,37 @@
 package com.android.classiccarselling.model;
 
+import java.util.List;
+import java.util.UUID;
+
 public class Car {
 
-
+    private String id;
 
     private Brand brand;
-    private String model;
+    private String name;
+
+    private String year;
+    private String registrationDate;
+    private String color;
+    private String kilometer;
+
     private double price;
 
-    public Car(Brand brand, String model, double price) {
+    private List<StorageImage> images;
+
+    public Car() {
+    }
+
+    public Car(Brand brand, String name, String year, String registrationDate, String color, String kilometer, double price, List<StorageImage> images) {
+        this.id = UUID.randomUUID().toString();
         this.brand = brand;
-        this.model = model;
+        this.name = name;
+        this.year = year;
+        this.registrationDate = registrationDate;
+        this.color = color;
+        this.kilometer = kilometer;
         this.price = price;
+        this.images = images;
     }
 
     public Brand getBrand() {
@@ -22,12 +42,44 @@ public class Car {
         this.brand = brand;
     }
 
-    public String getModel() {
-        return model;
+    public String getName() {
+        return name;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getKilometer() {
+        return kilometer;
+    }
+
+    public void setKilometer(String kilometer) {
+        this.kilometer = kilometer;
     }
 
     public double getPrice() {
@@ -36,5 +88,13 @@ public class Car {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public List<StorageImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<StorageImage> images) {
+        this.images = images;
     }
 }
