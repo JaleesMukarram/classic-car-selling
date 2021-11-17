c7343b77d7fecdd7b11666249f512e87b0885705
Jalees Mukarram <jaleesmukarram@gmail.com>
2021-11-18 01:17:18 +0500

    Storage item Added

---
+++ app/src/main/java/com/android/classiccarselling/model/StorageImage.java
@@ -0,0 +1,38 @@
+package com.android.classiccarselling.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+public class StorageImage extends StorageItem implements Parcelable, Serializable {
+
+
+    public StorageImage() {
+    }
+
+    public StorageImage(Parcel in) {
+        super(in);
+    }
+
+    public StorageImage(String name, String downloadURL, String completePath) {
+        super(name, downloadURL, completePath);
+    }
+
+    public static final Creator<StorageImage> CREATOR = new Creator<StorageImage>() {
+        @Override
+        public StorageImage createFromParcel(Parcel in) {
+            return new StorageImage(in);
+        }
+
+        @Override
+        public StorageImage[] newArray(int size) {
+            return new StorageImage[size];
+        }
+    };
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
