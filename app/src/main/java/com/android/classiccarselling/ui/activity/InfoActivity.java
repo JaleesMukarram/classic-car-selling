467e93273715fec8a318a623d1f4f4d085d68cc5
Jalees Mukarram <jaleesmukarram@gmail.com>
2021-11-19 15:44:34 +0500

    Personal Info Shown at Info Activity

--- app/src/main/java/com/android/classiccarselling/ui/activity/InfoActivity.java
+++ app/src/main/java/com/android/classiccarselling/ui/activity/InfoActivity.java
@@ -1,17 +1,68 @@
 package com.android.classiccarselling.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.classiccarselling.R;
+import com.android.classiccarselling.databinding.ActivityInfoBinding;
+import com.android.classiccarselling.interfaces.CustomHooks;
+import com.android.classiccarselling.utils.CommonUtils;
+import com.android.classiccarselling.viewmodel.ProfileVM;
 
-public class InfoActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity implements CustomHooks {
 
+    private ActivityInfoBinding binding;
+    private ProfileVM viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_info);
+
+        callHooks();
+    }
+
+    @Override
+    public void callHooks() {
+
+        initViews();
+        initListeners();
+        observe();
+    }
+
+    @Override
+    public void handleIntent() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+        viewModel = ViewModelProviders.of(this).get(ProfileVM.class);
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+
+    @Override
+    public void observe() {
+
+        viewModel.defaultUser.observe(this, defaultUser -> {
+
+            if (defaultUser != null) {
+
+                binding.tvName.setText(defaultUser.getName());
+                binding.tvCity.setText(defaultUser.getCity());
+                binding.tvPersonalInformation.setOnClickListener(view -> CommonUtils.changeActivity(this, ProfileActivity.class, true));
+            }
+        });
+
+        viewModel.getSavedUser();
     }
 }
